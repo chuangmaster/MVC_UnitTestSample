60c44f48cec17fabf4b4fc7be259427714ce0cd1
chuangmaster <chuangmaster@github.com>
2017-11-12 22:39:23 +0800

    安裝Nuget-moq套件

--- MVC_UnitTestSampleTestsByMoq/Controllers/NameControllerTests.cs
+++ MVC_UnitTestSampleTestsByMoq/Controllers/NameControllerTests.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVC_UnitTestSample.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MVC_UnitTestSampleTestsByMoq.Controllers
+namespace MVC_UnitTestSample.Controllers.Tests
 {
-    class NameControllerTests
+    [TestClass()]
+    public class NameControllerTests
     {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            Assert.Fail();
+        }
     }
 }
