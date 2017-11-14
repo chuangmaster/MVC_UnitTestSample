be1d0c79ee4ff673bd647a706464c88a176abd4e
chuangmaster <chuang.master@gmail.com>
2017-11-14 17:27:52 +0800

    單元測試-參數匹配器 http://www.cnblogs.com/gaochundong/archive/2013/05/22/nsubstitute_argument_matchers.html

--- UnitTest_NSubstitute/CalculatorTests.cs
+++ UnitTest_NSubstitute/CalculatorTests.cs
@@ -3,6 +3,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MVC_UnitTestSample.Helper.Interface;
 using NSubstitute;
 using NSubstitute.Exceptions;
+using System.Linq;
+using UnitTest_NSubstitute.Test.Interface;
 
 namespace UnitTest_NSubstitute
 {
@@ -319,6 +321,71 @@ namespace UnitTest_NSubstitute
             calculator.Received().Mode = "TEST";
         }
 
+        //Refer http://www.cnblogs.com/gaochundong/archive/2013/05/22/nsubstitute_argument_matchers.html
 
+
+        [TestMethod]
+        public void Test_ArgumentMatchers_IgnoringArguments()
+        {
+            //忽略參數
+            var calculator = Substitute.For<ICalculator>();
+
+            //忽略第一個參數
+            calculator.Add(Arg.Any<int>(), 5).Returns(7);
+
+            Assert.AreEqual(7, calculator.Add(42, 5));
+            Assert.AreEqual(7, calculator.Add(123, 5));
+            Assert.AreNotEqual(7, calculator.Add(1, 7));
+        }
+
+
+        [TestMethod]
+        public void Test_ArgumentMatchers_ConditionallyMatching()
+        {
+            //參數條件匹配
+            var calculator = Substitute.For<ICalculator>();
+
+            calculator.Add(1, -10);
+
+            // 检查接收到第一个参数为1，第二个参数小于0的调用
+            calculator.Received().Add(1, Arg.Is<int>(x => x < 0));
+            // 检查接收到第一个参数为1，第二个参数为 -2、-5和-10中的某个数的调用
+            calculator
+              .Received()
+              .Add(1, Arg.Is<int>(x => new[] { -2, -5, -10 }.Contains(x)));
+            // 检查未接收到第一个参数大于10，第二个参数为-10的调用
+            calculator.DidNotReceive().Add(Arg.Is<int>(x => x > 10), -10);
+        }
+
+        [TestMethod]
+        public void Test_ArgumentMatchers_ConditionallyMatchingThrowException()
+        {
+            //參數條件匹配 (注意Null)
+            IFormatter formatter = Substitute.For<IFormatter>();
+
+            formatter.Format(Arg.Is<string>(x => x.Length <= 10)).Returns("matched");
+
+            Assert.AreEqual("matched", formatter.Format("short"));
+            Assert.AreNotEqual("matched", formatter.Format("not matched, too long"));
+
+            // 此处将不会匹配，因为在尝试访问 null 的 Length 属性时会抛出异常，
+            // 而 NSubstitute 会假设其为不匹配并隐藏掉异常。
+            Assert.AreNotEqual("matched", formatter.Format(null));
+        }
+
+
+        [TestMethod]
+        public void Test_ArgumentMatchers_MatchingSpecificArgument()
+        {
+            //匹配指定的參數
+            var calculator = Substitute.For<ICalculator>();
+
+            calculator.Add(0, 42);
+
+            // 这里可能不工作，NSubstitute 在这种情况下无法确定在哪个参数上应用匹配器
+            //calculator.Received().Add(0, Arg.Any<int>());
+
+            calculator.Received().Add(Arg.Is(0), Arg.Any<int>());
+        }
     }
 }
